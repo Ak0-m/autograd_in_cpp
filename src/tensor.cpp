1e5cf15ba2a9661449012af1697bad0b89f4c3c6
Ak0-m <272337745+Ak0-m@users.noreply.github.com>
2026-09-13 21:24:48 +0200

    Implemented sum and mean operations

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -95,6 +95,51 @@ void Tensor::zero_grad()
     }
 }
 
+std::shared_ptr<Tensor> sum(const std::shared_ptr<Tensor> &x)
+{
+    double s = 0;
+
+    for (size_t i = 0; i < x->values().size(); i++)
+    {
+        s += x->values()[i];
+    }
+
+    std::vector<std::shared_ptr<Tensor>> prev = {x};
+
+    auto out = std::make_shared<Tensor>(std::vector<double>{s}, std::move(prev), "sum", std::vector<size_t>{1});
+
+    out->backward_func = [x, out]() { 
+        std::vector<double> contrib(x->values().size(), out->grads()[0]);
+        x->add_grad(contrib);
+    };
+
+    return out;
+}
+
+std::shared_ptr<Tensor> mean(const std::shared_ptr<Tensor> &x)
+{
+    double s = 0;
+
+    for (size_t i = 0; i < x->values().size(); i++)
+    {
+        s += x->values()[i];
+    }
+
+    s/=x->values().size();
+
+    std::vector<std::shared_ptr<Tensor>> prev = {x};
+
+    auto out = std::make_shared<Tensor>(std::vector<double>{s}, std::move(prev), "mean", std::vector<size_t>{1});
+
+    out->backward_func = [x, out]() { 
+        double scale = out->grads()[0] / static_cast<double>(x->values().size());
+        std::vector<double> contrib(x->values().size(), scale);
+        x->add_grad(contrib);
+    };
+
+    return out;
+}
+
 std::shared_ptr<Tensor> operator+(const std::shared_ptr<Tensor> &lhs, const std::shared_ptr<Tensor> &rhs)
 {
     if (lhs->shape() != rhs->shape())
@@ -141,16 +186,16 @@ std::shared_ptr<Tensor> operator*(const std::shared_ptr<Tensor> &lhs, const std:
 
     out->backward_func = [lhs, rhs, out]() {
         std::vector<double> contrib(out->values().size());
-        for(size_t i=0; i<contrib.size(); ++i)
+        for (size_t i = 0; i < contrib.size(); ++i)
         {
             contrib[i] = out->grads()[i] * rhs->values()[i];
         }
-        lhs -> add_grad(contrib);
-        for(size_t i=0; i<contrib.size(); ++i)
+        lhs->add_grad(contrib);
+        for (size_t i = 0; i < contrib.size(); ++i)
         {
             contrib[i] = out->grads()[i] * lhs->values()[i];
         }
-        rhs -> add_grad(contrib);
+        rhs->add_grad(contrib);
     };
 
     return out;
